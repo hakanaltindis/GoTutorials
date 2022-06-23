a2544ffbc140cee59d261419ddabc16a94cfe3d2
Hakan Altındiş <HakanA@d-teknoloji.com.tr>
2022-06-23 23:40:21 +0300

    - hello-world

---
+++ 01-HelloWorld/main.go
@@ -0,0 +1,12 @@
+// a package is a way group of functions and it's made up of all the files in the same directory
+package main
+
+// add dependency packages as using in C#
+import "fmt"
+
+// the main function executes by default when you run the main package
+func main() {
+
+	// write "Hello World!" message to console
+	fmt.Println("Hello World!")
+}
