3be284b51526dd4dc1b0c3fb0123ac78f03307e3
Hakan Altındiş <HakanA@d-teknoloji.com.tr>
2022-06-25 00:51:03 +0300

    - operators

---
+++ 04-Operators/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+/*
+ * To see details, click below the link
+ * https://www.tutorialspoint.com/go/go_operators.htm
+ */
+
+func main() {
+
+	// add two interger number
+	total := 20 + 12
+	fmt.Println(total)
+
+	// minus two integer number
+	result := 23 - 11
+	fmt.Println(result)
+
+	a := 20
+	b := 10
+
+	// add 1 to a variable
+	a++
+
+	// minus 1 from b variable
+	b--
+
+	// take mod
+	mod := a % b
+	fmt.Println(mod)
+
+}
